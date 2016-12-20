dc320efdc305d09a285ef0fc4f17ab9f1f1545c6
Mukesh A <amukesh.mk@gmail.com>
2016-12-20 12:31:05 +0530

    changing to implements
    changing to button implements

--- LabEx01/app/src/main/java/com/example/mk/labex01/MainActivity.java
+++ LabEx01/app/src/main/java/com/example/mk/labex01/MainActivity.java
@@ -3,16 +3,22 @@ package com.example.mk.labex01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b = (Button) findViewById(R.id.bt1);
+        b.setOnClickListener(this);
     }
-    void clickbtn(View v) {
+
+    @Override
+    public void onClick(View v) {
         Toast.makeText(this, "You KNOW to CLICK yey !", Toast.LENGTH_SHORT).show();
     }
 }
