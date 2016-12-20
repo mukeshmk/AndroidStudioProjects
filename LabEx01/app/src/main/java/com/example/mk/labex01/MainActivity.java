4bd29032d3c977af4f0a250f73e11c60b9cf5315
Mukesh A <amukesh.mk@gmail.com>
2016-12-20 12:51:44 +0530

    multiple buttons
    multiple buttons

--- LabEx01/app/src/main/java/com/example/mk/labex01/MainActivity.java
+++ LabEx01/app/src/main/java/com/example/mk/labex01/MainActivity.java
@@ -4,21 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button b;
+    Button b1, b2, b3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b = (Button) findViewById(R.id.bt1);
-        b.setOnClickListener(this);
+        b1 = (Button) findViewById(R.id.bt1);
+        b1.setOnClickListener(this);
+        b2 = (Button) findViewById(R.id.bt2);
+        b2.setOnClickListener(this);
+        b3 = (Button) findViewById(R.id.bt3);
+        b3.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "You KNOW to CLICK yey !", Toast.LENGTH_SHORT).show();
+        switch (v.getId()) {
+            case R.id.bt1:
+                Toast.makeText(this, "Clicked Button 1 !!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bt2:
+                Toast.makeText(this, "Clicked Button 2 !!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bt3:
+                Toast.makeText(this, "Clicked Button 3 !!", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
     }
 }
