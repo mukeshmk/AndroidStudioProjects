620f15567b11acbe723c3803f46bebea0d4cbbdb
Mukesh A <amukesh.mk@gmail.com>
2016-12-20 12:25:53 +0530

    Initial work
    textview and button with toast

---
+++ LabEx01/app/src/main/java/com/example/mk/labex01/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mk.labex01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    void clickbtn(View v) {
+        Toast.makeText(this, "You KNOW to CLICK yey !", Toast.LENGTH_SHORT).show();
+    }
+}
