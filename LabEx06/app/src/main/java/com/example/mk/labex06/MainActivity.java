d9b0db1ea360398dc15cfb3bd6ef1008fae627c4
Mukesh A <amukesh.mk@gmail.com>
2016-12-22 12:29:58 +0530

    Alert Complete
    Alert Complete
    Remaining: scroll view, toggle, and 1 more

--- LabEx06/app/src/main/java/com/example/mk/labex06/MainActivity.java
+++ LabEx06/app/src/main/java/com/example/mk/labex06/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mk.labex06;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,7 +17,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ProgressDialog Progress;
-    Button b1;
+    Button b1, b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         b1 = (Button) findViewById(R.id.progress);
         b1.setOnClickListener(this);
+
+        b2 = (Button) findViewById(R.id.alrt);
+        b2.setOnClickListener(this);
     }
 
     @Override
@@ -52,29 +58,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        Progress = new ProgressDialog(this);
-        Progress.setMessage("Progress Bar");
-        Progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        Progress.setIndeterminate(false);
-        Progress.setProgress(0);
-        Progress.show();
+        switch (v.getId()) {
 
-        final int TotalProgressTime = 100;
-        final Thread t = new Thread() {
-            public void run() {
-                int jumptime=0;
+            case R.id.progress:
+                Progress = new ProgressDialog(this);
+                Progress.setMessage("Progress Bar");
+                Progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                Progress.setIndeterminate(false);
+                Progress.setProgress(0);
+                Progress.show();
 
-                while(jumptime < TotalProgressTime) {
-                    try {
-                        sleep(200);
-                        jumptime += 5;
-                        Progress.setProgress(jumptime);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                final int TotalProgressTime = 100;
+                final Thread t = new Thread() {
+                    public void run() {
+                        int jumptime = 0;
+
+                        while (jumptime < TotalProgressTime) {
+                            try {
+                                sleep(200);
+                                jumptime += 5;
+                                Progress.setProgress(jumptime);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
                     }
-                }
-            }
-        };
-        t.start();
+                };
+                t.start();
+                break;
+            case R.id.alrt:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Alert Box");
+                builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Yes", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("no", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "no", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                break;
+        }
     }
 }
