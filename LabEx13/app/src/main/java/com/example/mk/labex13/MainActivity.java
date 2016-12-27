6fdb6d52e6435d10850a307644f358cd7f3e21ac
Mukesh A <amukesh.mk@gmail.com>
2016-12-27 14:35:11 +0530

    fab view
    floating action button view

---
+++ LabEx13/app/src/main/java/com/example/mk/labex13/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mk.labex13;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                Toast.makeText(this, "fab view!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
